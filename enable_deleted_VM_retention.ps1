e58971af11c88432bde1ed05117d5da172447f4e
Andrea Borella <bore.andrea@gmail.com>
2017-02-20 23:00:03 +0100

    Create enable_deleted_VM_retention.ps1

---
+++ enable_deleted_VM_retention.ps1
@@ -0,0 +1,8 @@
+asnp VeeamPsSnapin
+
+foreach ($job in Get-VBRJob){
+    $options = $job.GetOptions()
+    $options.EnableDeletedVmDataRetention = $true
+    $options.BackupStorageOptions.RetainDays = 5
+    $job.SetOptions($options) | Out-Null
+    }
