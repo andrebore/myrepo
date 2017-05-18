a70af1e1607c29cb0a1a1d89e93c119357876c54
Andrea Borella <bore.andrea@gmail.com>
2017-05-18 18:16:20 +0200

    sobr report

---
+++ Veeam/Reporting/sobr_rp-report.ps1
@@ -0,0 +1,27 @@
+#where's my restore points?
+#report per vedere in quale extent sono gli ultimi 3 restore point di una VM
+
+
+asnp VeeamPSSnapin
+
+foreach ($job in Get-VBRJob) {
+       $lastsession=$job.FindLastSession()
+       $sessionobjs=$lastsession.GetTaskSessions()
+
+       foreach ($vm in $sessionobjs.Name){
+             $rps=Get-VBRRestorePoint -Name $vm | Select -Last 3
+
+             $table = ForEach ($rp in $rps) {
+
+                [PSCustomObject]@{
+                     VM = $vm
+                      Extent = $rp.FindChainRepositories().Name
+                      RestorePoint = $rp.CreationTime
+                      Type = $rp.Type
+                   }
+             }
+
+             $table | Sort-Object -Property RestorePoint -Descending | Format-Table -AutoSize
+       }
+
+}
