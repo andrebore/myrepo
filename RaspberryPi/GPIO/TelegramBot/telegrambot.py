ec5bbeb06375df6a83ba2af193e59de2aacd9b2e
Andrea Borella <bore.andrea@gmail.com>
2017-05-21 11:42:08 +0200

    added telegrambot.py

---
+++ RaspberryPi/GPIO/TelegramBot/telegrambot.py
@@ -0,0 +1,39 @@
+import sys
+import time
+import random
+import datetime
+import telepot
+import RPi.GPIO as GPIO
+
+#LED
+def on(pin):
+    GPIO.output(pin,GPIO.HIGH)
+    return
+def off(pin):
+    GPIO.output(pin,GPIO.LOW)
+    return
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BCM)
+# set up GPIO output channel
+GPIO.setup(27, GPIO.OUT)
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    print 'Got command: %s' % command
+
+    if command == 'on':
+        bot.sendMessage(chat_id, on(27))
+    elif command =='off':
+        bot.sendMessage(chat_id, off(27))
+
+bot = telepot.Bot('350131512:AAFbqKgcn9i2cR597QfcxE_vrrA7GQL8F54')
+bot.message_loop(handle)
+print 'I am listening...'
+
+try:
+    while 1:
+        time.sleep(10)
+
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        GPIO.cleanup()
