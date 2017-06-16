33c6f3897629402423fb9b89f803155737c93f27
Andrea Borella <bore.andrea@gmail.com>
2017-06-16 11:43:59 +0200

    new app-aware scritp

---
+++ Veeam/BackupJob/enable_app-aware.ps1
@@ -0,0 +1,22 @@
+﻿asnp VeeamPsSnapin
+
+$VSSCredentials = get-vbrcredentials -name "FGCORP\A10046"
+
+
+foreach ($job in Get-VBRJob -name "Backup-*"){
+
+    $jobVSSOptions = Get-VBRJobVSSOptions -job $job
+    $jobVSSOptions.Enabled = $true
+
+    foreach ($jobObject in ($job | Get-VBRJobObject)){
+            $ObjectVSSOptions = Get-VBRJobObjectVssOptions -ObjectInJob $jobObject
+            $ObjectVSSOptions.IgnoreErrors = $true
+            $ObjectVSSOptions.VssSnapshotOptions.IsCopyOnly=$true
+            Set-VBRJobObjectVssOptions -Object $jobObject -Options $ObjectVSSOptions
+            }
+
+    Set-VBRJobVssOptions -job $job -Options $JobVSSOptions
+    Set-VBRJobVSSOptions -job $job -Credentials $VSSCredentials
+
+
+}
