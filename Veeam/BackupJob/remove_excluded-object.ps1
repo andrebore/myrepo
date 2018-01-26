12a4919bc4b864c5abf7971d1d2dc5429c2fed0b
Andrea Borella <bore.andrea@gmail.com>
2018-01-26 15:26:09 +0100

    corrected typos

--- Veeam/BackupJob/remove_excluded-object.ps1
+++ Veeam/BackupJob/remove_excluded-object.ps1
@@ -1,6 +1,6 @@
 asnp VeeamPsSnapIn
 
 foreach ($job in Get-VBRJob){
-    $obj=$job.GetObjectsInJob() | Where-Object {$_.Type -eq "Exclude"}
+    $obj=$job.GetObjectsInJob() | Where-Object {$_.Type -eq "Exclude" -and $_.Name -like "*whatever*"}
     $obj.Delete()
 }
