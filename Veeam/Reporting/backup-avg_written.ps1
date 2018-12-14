e6db0edd200ed0dfe4e4eeb724878a65f3f4824e
Andrea Borella <bore.andrea@gmail.com>
2018-12-14 16:48:56 +0100

    added new script

---
+++ Veeam/Reporting/backup-avg_written.ps1
@@ -0,0 +1,22 @@
+﻿asnp VeeamPSSnapin
+
+
+$table = @()
+
+foreach ($job in Get-VBRJob | Where-Object {$_.JobType -eq 'Backup'}){
+
+    $sessions=Get-VBRBackupSession | Where {$_.jobId -eq $job.Id.Guid} | Sort EndTimeUTC -Descending | Select -First 3
+    $sum = 0
+
+    foreach ($session in $sessions){
+        $sum += $session.Info.BackedUpSize
+    }
+
+    $data = New-Object System.Object
+    $data | Add-Member -type NoteProperty -Name 'Job' -Value $job.Name
+    $data | Add-Member -type NoteProperty -Name 'Avg. Written' -Value $(($sum / $sessions.count) / 1GB)
+    $table += $data
+        
+}
+
+$table
