4ece562412074dec52ca9b1ce57d5b0239975212
Andrea Borella <bore.andrea@gmail.com>
2019-04-18 15:06:09 +0200

    script to create group based on OU

---
+++ Veeam/VBO/o365_create-group-from-OU.ps1
@@ -0,0 +1,15 @@
+﻿$OU="OU=EMEA,OU=Utenti,DC=npo-torino,DC=local"
+$ShadowGroup="CN=DL-EMEA-ALL,OU=DistributionList,OU=Gruppi,OU=Utenti,DC=npo-torino,DC=local"
+#Get-ADGroupMember –Identity $ShadowGroup | Where-Object {$_.distinguishedName –NotMatch $OU} | ForEach-Object {Remove-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup –Confirm:$false}
+Get-ADGroupMember –Identity $ShadowGroup | ForEach-Object {Remove-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup –Confirm:$false}
+Get-ADUser –SearchBase $OU –LDAPFilter "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))" | ForEach-Object {Add-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup}
+
+$OU="OU=Consultant,OU=EMEA,OU=Utenti,DC=npo-torino,DC=local"
+$ShadowGroup="CN=DL-EMEA-CONSULTANT,OU=DistributionList,OU=Gruppi,OU=Utenti,DC=npo-torino,DC=local"
+Get-ADGroupMember –Identity $ShadowGroup  | ForEach-Object {Remove-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup –Confirm:$false}
+Get-ADUser –SearchBase $OU –LDAPFilter "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))" | ForEach-Object {Add-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup}
+
+$OU="OU=Employee,OU=EMEA,OU=Utenti,DC=npo-torino,DC=local"
+$ShadowGroup="CN=DL-EMEA-EMPLOYEE,OU=DistributionList,OU=Gruppi,OU=Utenti,DC=npo-torino,DC=local"
+Get-ADGroupMember –Identity $ShadowGroup  | ForEach-Object {Remove-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup –Confirm:$false}
+Get-ADUser –SearchBase $OU –LDAPFilter "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))" | ForEach-Object {Add-ADPrincipalGroupMembership –Identity $_ –MemberOf $ShadowGroup}
