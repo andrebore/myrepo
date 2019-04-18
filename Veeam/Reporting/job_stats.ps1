34224a1165d4a1375f655a55fc29d1605dd0259b
Andrea Borella <bore.andrea@gmail.com>
2019-04-18 15:05:13 +0200

    added job stats report

---
+++ Veeam/Reporting/job_stats.ps1
@@ -0,0 +1,16 @@
+﻿asnp VeeamPsSnapin
+
+$today=get-date
+
+foreach ($job in Get-VBRJob -name "SOBR_Backup02"){
+    
+    (Get-VBRBackup -Name "SOBR_Backup02").GetAllStorages() | where-object {$_.CreationTime -gt $today.AddDays(-1)} | Select-Object -Property `
+        @{N="BackupJob";E={$job.Name}},`
+        @{N="Name";E={$_.FilePath}},`
+        @{N="Date";E={$_.CreationTime}},`
+        @{N="Data Size";E={$_.Stats.DataSize}},`
+        @{N="Backup Size";E={$_.Stats.BackupSize}},`
+        @{N="De-dupe Ratio";E={$_.Stats.DedupRatio}},`
+        @{N="Compress Ratio";E={$_.Stats.CompressRatio}} |  Sort Date -Descending | Format-Table
+        
+        }
